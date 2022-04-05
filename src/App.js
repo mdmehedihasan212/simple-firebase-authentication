69ce7a39976b161ada1b5cfabcc360031a3fca1c
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-04-06 01:26:29 +0600

    add singout button and turnary operator into button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import app from './firebase.init';
-import { getAuth, signInWithPopup } from "firebase/auth";
+import { getAuth, signInWithPopup, signOut } from "firebase/auth";
 import { GoogleAuthProvider } from "firebase/auth";
 import { useState } from 'react';
 
@@ -22,13 +22,26 @@ function App() {
       })
   }
 
+  const handleToSingOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUsers({});
+      })
+      .catch((error) => {
+        setUsers({});
+      });
+  }
+
   return (
     <div className="App">
-      <button onClick={handleToSingIn}>Google sing in</button>
       <div className="users-container">
         <img style={{ 'width': '100px', 'margin-top': '30px' }} src={users.photoURL} alt="" />
-        <h3>{users.displayName}</h3>
-        <p>{users.email}</p>
+        <h3>Name: {users.displayName}</h3>
+        <p>E-mail: {users.email}</p>
+        {users.email ?
+          <button onClick={handleToSingOut}>Sing out</button> :
+          <button onClick={handleToSingIn}>Google sing in</button>
+        }
       </div>
     </div>
   );
